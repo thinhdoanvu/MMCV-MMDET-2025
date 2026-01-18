9f00deaf93d8df924ed1808d08851ce54dfe61fe
thinhdoanvu <thinhdv@ntu.edu.vn>
2026-01-18 14:56:18 +0800

    Add files via upload

--- Utils/config_aicup25_dino.py
+++ Utils/config_aicup25_dino.py
@@ -1,20 +1,23 @@
-'''
-Swin-Tiny
-Swin-Small
-Swin-Base
+_base_ = [
+    'mmdet::_base_/datasets/coco_detection.py',
+    'mmdet::_base_/default_runtime.py'
+]
 
-This is Medium/Base version
-'''
-_base_ = 'dynamic_rcnn/dynamic-rcnn_r50_fpn_1x_coco.py'  # Dùng base schedule, runtime từ dynamic rcnn
 pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth'
 
-dataset_type = 'CocoDataset'
-classes = ('aortic_valve',)
-data_root = 'data/coco/'
-
+# 1. MODEL CONFIGURATION
 model = dict(
+    type='DINO',
+    num_queries=900,
+    with_box_refine=True,
+    as_two_stage=True,
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_size_divisor=1),
     backbone=dict(
-        _delete_=True,
         type='SwinTransformer',
         embed_dims=128,
         depths=[2, 2, 18, 2],
@@ -22,77 +25,139 @@ model = dict(
         window_size=7,
         mlp_ratio=4,
         qkv_bias=True,
-        drop_path_rate=0.5,
+        drop_path_rate=0.2,
         patch_norm=True,
-        out_indices=(0, 1, 2, 3),
+        out_indices=(1, 2, 3),
         with_cp=False,
         convert_weights=True,
-        init_cfg=dict(type='Pretrained', checkpoint=pretrained)
-    ),
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
     neck=dict(
-        type='FPN',
-        in_channels=[128, 256, 512, 1024],
+        type='ChannelMapper',
+        in_channels=[256, 512, 1024],
+        kernel_size=1,
         out_channels=256,
-        num_outs=5
-    ),
-    roi_head=dict(
-        bbox_head=dict(num_classes=len(classes))  # để đếm tự động
-    )
-)
+        act_cfg=None,
+        norm_cfg=dict(type='GN', num_groups=32),
+        num_outs=4),
+    encoder=dict(
+        num_layers=6,
+        layer_cfg=dict(
+            self_attn_cfg=dict(embed_dims=256, num_levels=4, dropout=0.0),
+            ffn_cfg=dict(
+                embed_dims=256,
+                feedforward_channels=2048,
+                ffn_drop=0.0,
+                act_cfg=dict(type='ReLU', inplace=True)))),
+    decoder=dict(
+        num_layers=6,
+        return_intermediate=True,
+        layer_cfg=dict(
+            self_attn_cfg=dict(embed_dims=256, num_heads=8, dropout=0.0),
+            cross_attn_cfg=dict(embed_dims=256, num_levels=4, dropout=0.0),
+            ffn_cfg=dict(
+                embed_dims=256,
+                feedforward_channels=2048,
+                ffn_drop=0.0,
+                act_cfg=dict(type='ReLU', inplace=True))),
+        post_norm_cfg=None),
+    positional_encoding=dict( # THÊM BLOCK NÀY ĐỂ SỬA LỖI
+        num_feats=128,
+        normalize=True,
+        offset=0.0,
+        temperature=20),
+    bbox_head=dict(
+        type='DINOHead',
+        num_classes=1,
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=5.0),
+        loss_iou=dict(type='GIoULoss', loss_weight=2.0)),
+    dn_cfg=dict(
+        label_noise_scale=0.5,
+        box_noise_scale=1.0,
+        group_cfg=dict(dynamic=True, num_groups=None, num_dn_queries=100)),
+    train_cfg=dict(
+        assigner=dict(
+            type='HungarianAssigner',
+            match_costs=[
+                dict(type='FocalLossCost', weight=2.0),
+                dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
+                dict(type='IoUCost', iou_mode='giou', weight=2.0)
+            ])),
+    test_cfg=dict(max_per_img=300))
 
-backend_args = None
+# 2. DATASET CONFIGURATION
+dataset_type = 'CocoDataset'
+classes = ('aortic_valve',)
+data_root = 'data/coco/'
+
 train_pipeline = [
-    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RandomFlip', prob=0.5),
     dict(type='Resize', scale=(640, 640), keep_ratio=True),
-    # dict(type='RandomFlip', prob=0.5),  # bật nếu muốn augment
     dict(type='PackDetInputs')
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadImageFromFile'),
     dict(type='Resize', scale=(640, 640), keep_ratio=True),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='PackDetInputs',
-        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+    dict(type='PackDetInputs', meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'))
 ]
 
 train_dataloader = dict(
     batch_size=4,
     num_workers=4,
-    persistent_workers=False,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
         type=dataset_type,
-        test_mode=False,
         metainfo=dict(classes=classes),
         data_root=data_root,
         ann_file='annotations/instances_train2017.json',
         data_prefix=dict(img='images/'),
-        pipeline=train_pipeline
-    )
-)
+        pipeline=train_pipeline))
 
 val_dataloader = dict(
-    batch_size=4,
-    num_workers=4,
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         type=dataset_type,
-        test_mode=True,
         metainfo=dict(classes=classes),
         data_root=data_root,
         ann_file='annotations/instances_val2017.json',
         data_prefix=dict(img='images/'),
-        pipeline=test_pipeline
-    )
-)
+        pipeline=test_pipeline))
 
+val_evaluator = dict(type='CocoMetric', ann_file=data_root + 'annotations/instances_val2017.json', metric='bbox')
+test_evaluator = val_evaluator
+
+# 3. OPTIMIZER & SCHEDULER
+optim_wrapper = dict(
+    type='AmpOptimWrapper',
+    loss_scale='dynamic',
+    optimizer=dict(type='AdamW', lr=0.0001, weight_decay=0.0001),
+    clip_grad=dict(max_norm=0.1, norm_type=2),
+    paramwise_cfg=dict(custom_keys={'backbone': dict(lr_mult=0.1)}))
+
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=100, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+param_scheduler = [
+    dict(type='MultiStepLR', begin=0, end=100, by_epoch=True, milestones=[80, 95], gamma=0.1)
+]
+
+# 4. RUNTIME
 default_hooks = dict(
-    checkpoint=dict(
-        type='CheckpointHook',
-        save_best='coco/bbox_mAP',
-        rule='greater'
-    )
-)
+    checkpoint=dict(type='CheckpointHook', interval=5, save_best='coco/bbox_mAP', rule='greater', max_keep_ckpts=3),
+    logger=dict(type='LoggerHook', interval=50))
 
-train_cfg = dict(max_epochs=100)
-runner = dict(type='EpochBasedRunner', max_epochs=100)
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = dict(type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
